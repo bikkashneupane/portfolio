7b99d44d5cd59fab93df19b738a0c33c21cf38c0
bikashneupane17 <edubikkash@gmail.com>
2024-07-18 23:11:17 +1000

    Footer responsive fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-800 text-gray-300 flex items-center justify-center min-h-[180px] text-lg font-mono tracking-wider">
+    <footer className="px-2 bg-gray-800 text-gray-300 flex items-center justify-center min-h-[180px] text-lg font-mono tracking-wider">
       {currentYear} &copy; Bikash Neupane | All Rights Reserved
     </footer>
   );
